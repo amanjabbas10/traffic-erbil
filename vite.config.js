9e3a3213032e3c3ab3bd5a799bbba12e01497395
amanjabbas <amanjabbas4.7@gmail.com>
2026-03-13 12:31:24 -0700

    Add new feature

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/traffic-erbil/',
   plugins: [react()],
   base: '/traffic-erbil/',
 })
